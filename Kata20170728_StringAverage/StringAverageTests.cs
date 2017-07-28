9c9ffe612c0200140e2a5c43620a021d1278e027
cash <cash@cashwu.com>
2017-07-28 13:28:35 +0800

    empty string test case

--- Kata20170728_StringAverage/StringAverageTests.cs
+++ Kata20170728_StringAverage/StringAverageTests.cs
@@ -7,8 +7,19 @@ namespace Kata20170728_StringAverage
     public class StringAverageTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_empty_should_return_na()
         {
+            var kata = new Kata();
+            var actual = kata.AverageString("");
+            Assert.AreEqual("n/a", actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string AverageString(string str)
+        {
+            return "n/a";
         }
     }
 }
