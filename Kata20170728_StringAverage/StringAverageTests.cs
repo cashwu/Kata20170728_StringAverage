2f37cf7c984f60216b4f6839960b59760c50e78e
cash <cash@cashwu.com>
2017-07-28 14:21:59 +0800

    one one test case

--- Kata20170728_StringAverage/StringAverageTests.cs
+++ Kata20170728_StringAverage/StringAverageTests.cs
@@ -13,13 +13,26 @@ namespace Kata20170728_StringAverage
             var actual = kata.AverageString("");
             Assert.AreEqual("n/a", actual);
         }
+
+        [TestMethod]
+        public void input_one_one_should_return_one()
+        {
+            var kata = new Kata();
+            var actual = kata.AverageString("one one");
+            Assert.AreEqual("one", actual);
+        }
     }
 
     public class Kata
     {
         public string AverageString(string str)
         {
-            return "n/a";
+            if (string.IsNullOrEmpty(str))
+            {
+                return "n/a";
+            }
+
+            return "one";
         }
     }
 }
